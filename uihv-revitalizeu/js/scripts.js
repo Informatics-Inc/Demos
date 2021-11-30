7bccdaf495109c4fa12f1cc65858314a9ddf3e3e
Jeff Stephens <jeffrey.stephens@informaticsinc.com>
2021-11-30 12:46:43 -0500

    revitalize prototype

---
+++ uihv-revitalizeu/js/scripts.js
@@ -0,0 +1,41 @@
+// --- SWUP TRANSITIONS --- //
+$(document).ready(function(){
+	$(window).scroll(function() {
+	   var x = $(this).scrollTop();
+	   $(".parallax-2").css("-webkit-transform","translateY(" +  (x/5)  + "px)");
+
+	   var scroll = $(window).scrollTop();
+		 if (scroll > 75) {
+			 $("#header").addClass("scroll");
+		 } else {
+			 $("#header").removeClass("scroll");
+		 }
+		 
+   });
+
+   // SWIPER  //
+	if (document.querySelector('.carousel')) {
+		var swiper2 = new Swiper('.carousel', {
+			slidesPerView: 1,
+			autoplay: {
+				delay: 3000,
+			},
+			speed: 1000,
+			loop: true,
+			pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+			},
+			breakpoints: {
+				600: {
+					slidesPerView: 3,
+					spaceBetween: 25,
+					},
+				1024: {
+				slidesPerView: 4,
+				spaceBetween: 25,
+				}
+			}
+		});
+	}
+});
