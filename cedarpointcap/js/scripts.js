2858cb378d8fc567d5d844a2232d121601b6ea11
Jeff Stephens <jeffrey.stephens@informaticsinc.com>
2021-12-24 11:05:49 -0500

    cedarpointcap move out

---
+++ cedarpointcap/js/scripts.js
@@ -0,0 +1,90 @@
+
+// For Local Dev Load Only //
+$(function(){
+	$("#load-header").load("header.html");
+	$("#load-footer").load("footer.html");
+});
+
+$("#menu-toggle" ).click(function(){
+	$("#menu").toggleClass("active");
+	$("#menu-toggle").toggleClass("active");
+});
+$('#menu li .toggle').click(function() {
+	$('#menu li.active').removeClass('active');
+	$(this).parent().addClass('active');
+});
+
+$('#menu li .toggle').click(function() {
+	$('#menu li.active').removeClass('active');
+	$(this).parent().addClass('active');
+});
+
+// -- ADD CLASS ON SCROLL --//
+$(window).scroll(function() {    
+	var scroll = $(window).scrollTop();
+	if (scroll > 50) {
+		$("#site-header").addClass("scroll");
+	} else {
+		$("#site-header").removeClass("scroll");
+	}
+	var x = $(this).scrollTop();
+	   $(".header img").css("-webkit-transform","translateY(" +  (x/5)  + "px)");
+  });
+
+// -- ANIMATE IN TO VIEW -- //
+var $animation_elements = $('.animate-in, .btn-text');
+var $window = $(window);
+
+function check_if_in_view() {
+var window_height = $window.height();
+var window_top_position = $window.scrollTop();
+var window_bottom_position = (window_top_position + window_height);
+
+$.each($animation_elements, function() {
+	var $element = $(this);
+	var element_height = $element.outerHeight();
+	var element_top_position = $element.offset().top;
+	var element_bottom_position = (element_top_position + element_height);
+
+	//check to see if this current container is within viewport
+	if ((element_bottom_position >= window_top_position) &&
+	(element_top_position <= window_bottom_position)) {
+	$element.addClass('visible');
+	} else {
+	
+	}
+});
+}
+
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
+
+// SWIPER Who We Are Values //
+if (document.querySelector('.swiper-container')) {
+	var values = ['Fudiciary Grade', 'Fee Only', 'Evidenced Based', 'Relationship Driven']
+	var swiper = new Swiper('.swiper-container', {
+		slidesPerView: 'auto',
+		speed: 1050,
+		slidesPerView: 1,
+		centeredSlides: true,
+		loop: true,
+		autoplay: true,
+		spaceBetween: 50,
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+			renderBullet: function (index, className) {
+			return '<li class="' + className + '">' + (values[index]) + '</li>';
+			},
+		},
+		navigation: {
+			nextEl: '.swiper-next',
+			prevEl: '.swiper-prev',
+		},
+	});
+}
+$(".swiper-container").hover(function() {
+    (this).swiper.autoplay.stop();
+  }, function() {
+    (this).swiper.autoplay.start();
+  });
