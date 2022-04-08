754ef0e8bd6f5cfc9d30838c9a04c39259d57556
Jeff Stephens <jeffrey.stephens@informaticsinc.com>
2022-04-08 16:03:42 -0400

    ABOMS app

---
+++ abom-app-prototype/js/scripts.js
@@ -0,0 +1,17 @@
+// --- SWUP TRANSITIONS --- //
+$(document).ready(function(){
+	// Toggle Menu
+	$(".menu-toggle" ).click(function(){
+		$("#main-menu").toggleClass("active");
+	});
+
+//	$('#menu li .toggle').click(function() {
+//		$('#menu li.active').removeClass('active');
+//		$(this).parent().addClass('active');
+//	});
+//	
+//	$('#menu li .toggle').click(function() {
+//		$('#menu li.active').removeClass('active');
+//		$(this).parent().addClass('active');
+//	});
+});
