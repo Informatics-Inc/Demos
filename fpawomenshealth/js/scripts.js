a0c04f2cd6f84e6c8b8b76c00ef3822a56433d84
Jeff Stephens <jeffrey.stephens@informaticsinc.com>
2022-04-15 16:03:48 -0400

    JS theme updates

---
+++ fpawomenshealth/js/scripts.js
@@ -0,0 +1,8 @@
+// --- SWUP TRANSITIONS --- //
+$(document).ready(function(){
+	   $(window).scroll(function() {
+		  var x = $(this).scrollTop();
+		  $(".header img").css("-webkit-transform","translateY(" +  (x/3)  + "px)");
+		  $(".parallax-2").css("-webkit-transform","translateY(" +  (x/5)  + "px)");
+	  });
+});
